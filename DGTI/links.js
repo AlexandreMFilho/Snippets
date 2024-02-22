02030bbfda345c07d34a0b1fbd4f84b8ac0048e9
filho.alexandre <filho.alexandre@graduacao.uerj.br>
2024-02-22 15:59:48 -0300

    subiu

--- DGTI/links.js
+++ DGTI/links.js
@@ -1,10 +1,5 @@
-
 <script src="../assets/bootstrap-5.3.2-dist/js/popper.min.js"></script>
-    
 <script src="../assets/bootstrap-5.3.2-dist/js/bootstrap.js" ></script>
-   
 <script src="../assets/DataTables/jquery-3.3.1.js"></script>
-
 <script src="../assets/DataTables/datatables.min.js"></script>
-
 <script type="module" src="AlunoExcedentesAposRID.js" ></script>
