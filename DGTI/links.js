39c0c2d8f11d3e701d0c378bd794efbb4d625755
filho.alexandre <filho.alexandre@graduacao.uerj.br>
2024-02-22 15:16:26 -0300

    Add varios snippets

---
+++ DGTI/links.js
@@ -0,0 +1,10 @@
+
+<script src="../assets/bootstrap-5.3.2-dist/js/popper.min.js"></script>
+    
+<script src="../assets/bootstrap-5.3.2-dist/js/bootstrap.js" ></script>
+   
+<script src="../assets/DataTables/jquery-3.3.1.js"></script>
+
+<script src="../assets/DataTables/datatables.min.js"></script>
+
+<script type="module" src="AlunoExcedentesAposRID.js" ></script>
